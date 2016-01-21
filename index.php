a5d376b5d20231e8c3391e35020641950060dc6a
CH-Aric <aric@coolheatcomfort.ca>
2016-01-21 12:55:33 -0500

    Index Creation

---
+++ index.php
@@ -0,0 +1,40 @@
+
+
+<?php
+echo "<html>
+		<head>
+			<title>CRM 2</title>
+			<link rel='stylesheet' type='text/css' href='/chat.css'>
+		</head>
+		<body>";
+session_start();
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
